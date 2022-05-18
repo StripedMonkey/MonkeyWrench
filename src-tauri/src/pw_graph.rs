a20ee16c214422fc44f35c29b27bc1dcee7c34f3
StripedMonkey <monkeyinastripedshirt@gmail.com>
2022-05-18 02:59:32 -0400

    prototype serialization objects for graph

--- src-tauri/src/pw_graph.rs
+++ src-tauri/src/pw_graph.rs
@@ -1,5 +1,36 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+enum ChannelType {
+    Audio,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+struct NodeEvent {
+    object_id: u32,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+struct PortEvent {
+    object_id: u32,
+    channel_type: ChannelType,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+struct WireEvent {
+    object_id: u32,
+
+    input_node_id: u32,
+    output_node_id: u32,
+
+    output_port_id: u32,
+    input_port_id: u32,
+}
 
 
+
+struct PWManager {
+}
 
 #[cfg(test)]
 mod test {
