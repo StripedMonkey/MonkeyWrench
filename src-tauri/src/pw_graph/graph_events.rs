c2664d9fb94de8051c58c1b61cf27386c6676dd0
StripedMonkey <monkeyinastripedshirt@gmail.com>
2022-06-15 14:25:53 -0400

    Improve parsing of wire events

--- src-tauri/src/pw_graph/graph_events.rs
+++ src-tauri/src/pw_graph/graph_events.rs
@@ -1,6 +1,6 @@
 use std::str::FromStr;
 
-use log::{warn, trace};
+use log::trace;
 use pipewire::{prelude::ReadableDict, registry::GlobalObject, spa::ForeignDict};
 use serde::{Deserialize, Serialize};
 
@@ -21,7 +21,8 @@ impl FromStr for PortDirection {
     type Err = (); // TODO: Actually do this
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s.to_lowercase().as_str() { // Probably don't need to have it be lowercase
+        match s.to_lowercase().as_str() {
+            // Probably don't need to have it be lowercase
             "in" => Ok(PortDirection::In),
             "out" => Ok(PortDirection::Out),
             _ => todo!(),
@@ -44,10 +45,8 @@ pub(crate) struct PortEvent {
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub(crate) struct NodeEvent {
     object_id: u32,
-    application_name: String,
+    application_name: Option<String>, // Optional because the dummy driver doesn't have an Application Name
     node_name: String,
-    
-
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
@@ -72,9 +71,9 @@ pub(crate) enum GraphEvent {
 // I need a better way to do this.
 pub(crate) fn get_event(pw_event: &GlobalObject<ForeignDict>) -> Result<GraphEvent, ()> {
     match &pw_event.type_ {
-        pipewire::types::ObjectType::Node => Ok(GraphEvent::Node(parse_node_event(pw_event))),
-        pipewire::types::ObjectType::Port => Ok(GraphEvent::Port(parse_port_event(pw_event))),
-        pipewire::types::ObjectType::Link => Ok(GraphEvent::Wire(parse_wire_event(pw_event))),
+        pipewire::types::ObjectType::Node => parse_node_event(pw_event),
+        pipewire::types::ObjectType::Port => parse_port_event(pw_event),
+        pipewire::types::ObjectType::Link => parse_wire_event(pw_event),
 
         other => {
             trace!("Unhandled Object Type {:?}", &other);
@@ -102,36 +101,36 @@ pub(crate) fn get_event(pw_event: &GlobalObject<ForeignDict>) -> Result<GraphEve
     }
 }
 
-fn parse_port_event(pw_event: &GlobalObject<ForeignDict>) -> PortEvent {
+fn parse_port_event(pw_event: &GlobalObject<ForeignDict>) -> Result<GraphEvent, ()> {
     let properties = pw_event.props.as_ref().unwrap();
-    PortEvent {
+    Ok(GraphEvent::Port(PortEvent {
         object_id: pw_event.id,
         node_id: properties.get("node.id").unwrap().parse().unwrap(),
         direction: properties.get("port.direction").unwrap().parse().unwrap(),
-    }
+    }))
 }
 
-fn parse_wire_event(pw_event: &GlobalObject<ForeignDict>) -> WireEvent {
+fn parse_wire_event(pw_event: &GlobalObject<ForeignDict>) -> Result<GraphEvent, ()> {
     let properties = pw_event.props.as_ref().unwrap();
-    WireEvent {
+    Ok(GraphEvent::Wire(WireEvent {
         object_id: pw_event.id,
         input_node_id: properties.get("link.input.node").unwrap().parse().unwrap(),
         input_port_id: properties.get("link.input.port").unwrap().parse().unwrap(),
         output_node_id: properties.get("link.output.node").unwrap().parse().unwrap(),
         output_port_id: properties.get("link.output.port").unwrap().parse().unwrap(),
-    }
+    }))
 }
 
-fn parse_node_event(pw_event: &GlobalObject<ForeignDict>) -> NodeEvent {
+fn parse_node_event(pw_event: &GlobalObject<ForeignDict>) -> Result<GraphEvent, ()> {
     let properties = pw_event.props.as_ref().unwrap();
-    NodeEvent {
+    Ok(GraphEvent::Node(NodeEvent {
         object_id: pw_event.id,
-        application_name: properties.get("application.name").unwrap().into(),
+        application_name: properties
+            .get("application.name")
+            .and_then(|s| Some(s.into())),
         node_name: properties.get("node.name").unwrap().into(),
-    }
+    }))
 }
 
 #[cfg(test)]
-mod test {
-
-}
+mod test {}
